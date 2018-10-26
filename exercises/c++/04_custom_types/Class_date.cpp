5de143bc32660469a2db3eadd4b1fc9a7b322335
MarcoAlbertoGrimaldi <marco.alberto.grimaldi@gmail.com>
2018-10-26 17:23:48 +0200

    updated ex_4

--- exercises/c++/04_custom_types/Class_date.cpp
+++ exercises/c++/04_custom_types/Class_date.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <array>
-#include <vector> 
+#include <ctime> 
 
 enum class month {jan=1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec};
 
@@ -9,22 +9,30 @@ month& operator++ (month& m) {
     return m;
 }
 
+
+// std::istream& operator>>(std::istream& is, month& m) {
+    
+// };
+
+bool is_leap(const int y);
+
 class Date {
+
+    public:
 
     unsigned int _day;
     month _month;
     unsigned int _year;
 
-    public:
-
-     Date(const unsigned int d, const month m, const unsigned int y)
-     : _day{d},
+     Date(const unsigned int date, const month m, const unsigned int y)
+     : _day{date},
       _month{m},
       _year{y}{}
 
+     Date();
      ~Date() {};
 
-     const unsigned int day() {
+     const unsigned int get_day() {
      return _day;
      }
 
@@ -32,8 +40,20 @@ class Date {
      return _month;
      }
 
-     const unsigned int year() {
+     const unsigned int get_year() {
      return _year;
+     }
+
+     void set_day(Date& date, const unsigned int d) {
+         date._day = d;
+     }
+
+     void set_month(Date& date, month m) {
+         date._month= m;
+     }
+
+     void set_year(Date& date, const unsigned int y) {
+         date._year= y;
      }
 
      void add_day();
@@ -42,18 +62,69 @@ class Date {
      
 };
 
+std::ostream& operator<<(std::ostream& os, const Date& d) {
+    const char* months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    os << d._day << "-" << months[int(d._month)-1] << "-" << d._year << std::endl;
+    return os;
+};
+
+bool operator== (const Date& lhs,const Date& rhs) {
+
+    if (lhs._day==rhs._day && lhs._month==rhs._month && lhs._year==rhs._year) {
+        return true;}
+    else return false;
+};
+
+bool operator!= (const Date& lhs, const Date& rhs) {
+    if (lhs==rhs) {return false;}
+    else return true;
+};
+
 
 int main()
 {
-    Date d {4, month::mar, 2018};
+    std::time_t t = std::time(0);   // get time now
+    std::tm* now = std::localtime(&t);
+    std::cout << "Today date: ";
+    std::cout <<  now->tm_mday << '-' << (now->tm_mon + 1) << '-' << (now->tm_year + 1900) << "\n";
 
-    unsigned int n{};
+    unsigned int d = (now->tm_mday), m = (now->tm_mon +1), y = (now->tm_year + 1900);
+    Date date {d, month(m), y};
 
-    std::cin >> n;
+    unsigned int l_d, l_mt, l_y, r_d, r_mt, r_y;
+    month l_m, r_m;
+   
+    unsigned int n{}, action{};
 
-    d.add_days(n);
+    std::cout << "Press: 0 to add days, 1 for compare dates, 2 for quit.\n";
+    std::cin >> action;
 
-    std::cout << d.day() << " " << int(d.get_month()) << " " << d.year() << std::endl;
+    switch(action) {
+        case 0:
+        std::cout << "Insert how many days you want to add: " << std::endl;
+        std::cin >> n;
+        date.add_days(n);
+        std::cout << "New changed date is: " << date << std::endl;
+        break;
+
+        case 1: {   
+        std::cout << "Insert the first date as dd mm yyyy: " << std::endl;
+        std::cin >> l_d >> l_mt >> l_y;
+        l_m = month(l_mt);
+        const Date l_date{l_d, l_m, l_y};
+
+        std::cout << "Insert the second date as dd mm yyyy: " << std::endl;
+        std::cin >> r_d >> r_mt >> r_y;
+        r_m = month(r_mt);
+        const Date r_date{r_d, r_m, r_y};
+
+        if (l_date==r_date) {std::cout << "The two given dates are the same.\n";}
+        else {std::cout << "The two given dates are different.\n";}
+        break;
+        }
+        case 2:
+        break;
+    }
 
     return 0;
 }
@@ -87,6 +158,5 @@ void Date::add_day() {
         _year++;
         _day = 1;
       }
-
-}
+    }
 }
